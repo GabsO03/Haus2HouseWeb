e3935fb04f4d2304e545179ee74aaee33c8a01f3
Gabriela Oria <gaboripin@gmail.com>
2025-06-04 21:31:06 +0200

    Intento de despliegue 5

--- replace-env.js
+++ replace-env.js
@@ -10,4 +10,4 @@ export const environment = {
 };
 `;
 
-fs.writeFileSync('src/environments/environment.prod.ts', envConfig);
+fs.writeFileSync('src/app/environments/environment.prod.ts', envConfig);
